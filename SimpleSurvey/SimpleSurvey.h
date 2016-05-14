ede2ad01a8f21bce3eb7e235d4415096c1637eb3
Ben Norris <ben@bsn.io>
2016-05-14 16:12:54 -0600

    Add blank framework

---
+++ SimpleSurvey/SimpleSurvey.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSurvey.h
+//  SimpleSurvey
+//
+//  Created by Ben Norris on 5/14/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSurvey.
+FOUNDATION_EXPORT double SimpleSurveyVersionNumber;
+
+//! Project version string for SimpleSurvey.
+FOUNDATION_EXPORT const unsigned char SimpleSurveyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSurvey/PublicHeader.h>
+
+
